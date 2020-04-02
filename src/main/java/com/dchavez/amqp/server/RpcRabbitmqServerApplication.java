ec8f473c0860c430c58b9806428e5547b2dcb0b1
Diego Chavez <diegochavez@diegochavez.local>
2020-04-02 16:11:22 -0300

    dead letter andando con AmqpRejectAndDontRequeueException pero sin ttl

--- src/main/java/com/dchavez/amqp/server/RpcRabbitmqServerApplication.java
+++ src/main/java/com/dchavez/amqp/server/RpcRabbitmqServerApplication.java
@@ -9,6 +9,7 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 
@@ -19,15 +20,15 @@ public class RpcRabbitmqServerApplication {
 		SpringApplication.run(RpcRabbitmqServerApplication.class, args);
 	}
 
-//	@Bean
-//	public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
-//		return new RabbitTemplate(connectionFactory);
-//	}
-
 	@Bean
-	public Queue queue() {
-		return new Queue("tut.rpc.requests");
+	public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+		return new RabbitTemplate(connectionFactory);
 	}
+
+//	@Bean
+//	public Queue queue() {
+//		return new Queue("tut.rpc.requests");
+//	}
 
 	@Bean
 	public DirectExchange exchange() {
@@ -37,6 +38,21 @@ public class RpcRabbitmqServerApplication {
 	@Bean
 	public Binding binding(DirectExchange exchange, Queue queue) {
 		return BindingBuilder.bind(queue).to(exchange).with("rpc");
+	}
+	
+	@Bean
+	public Queue queue() {
+	    return QueueBuilder.durable("tut.rpc.requests")
+	      //.withArgument("x-message-ttl", 3000)
+	      .withArgument("x-dead-letter-exchange", "")
+	      .withArgument("x-dead-letter-routing-key", "tut.rpc.requests.dlq")
+	      .ttl(3000)
+	      .build();
+	}
+	  
+	@Bean
+	Queue deadLetterQueue() {
+	    return QueueBuilder.durable("tut.rpc.requests.dlq").build();
 	}
 
 }
--- src/main/java/com/dchavez/amqp/server/services/RpcServer.java
+++ src/main/java/com/dchavez/amqp/server/services/RpcServer.java
@@ -1,7 +1,7 @@
 package com.dchavez.amqp.server.services;
 
 import org.springframework.stereotype.Service;
-
+import org.springframework.amqp.AmqpRejectAndDontRequeueException;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +18,8 @@ public class RpcServer {
 	@RabbitListener(queues = "tut.rpc.requests")
 	// @SendTo("tut.rpc.replies") used when the client doesn't set replyTo.
 	public int fibonacci(int n) {
+		if (n>43)
+			throw new AmqpRejectAndDontRequeueException("se rechaza!");
 		logger.debug(" [x] Received request for " + n);
 		int result = fib(n);
 		logger.debug(" [.] Returned " + result);
